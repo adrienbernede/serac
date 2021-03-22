06249ed566dc2d0784904eb9f09f7912b7054628
Josh Essman <essman1@llnl.gov>
2021-03-22 12:23:25 -0500

    cleanup: use repo dir for mesh path

--- tests/mesh_bug.cpp
+++ tests/mesh_bug.cpp
@@ -7,10 +7,12 @@ void bar(std::vector<int>& vec)
   foo(vec);
 }
 
+#include "serac/serac_config.hpp" // for SERAC_REPO_DIR
+
 int main(int argc, char* argv[])
 {
   MPI_Init(&argc, &argv);
-  std::ifstream imesh("/home/essman1/sandbox/serac/data/meshes/beam-hex.mesh");
+  std::ifstream imesh(SERAC_REPO_DIR"/data/meshes/beam-hex.mesh");
   mfem::Mesh serial(imesh, 1, 1, true);
   mfem::ParMesh parallel(MPI_COMM_WORLD, serial);
   std::cout << parallel.GetNE() << "\n";
